ec665d47d6c6eb96b13cce74d16342db8069edca
NicoDev <nicogabrieloni@gmail.com>
2023-07-27 17:48:28 -0300

    uber-4

---
+++ uber-4.ts
@@ -0,0 +1,18 @@
+function solution(l: number, fares: number[]) {
+
+    let resultado = fares.map((item) => item * l);
+    
+    if (resultado[4] <= 20) {
+        return "UberSUV";
+    } else if (resultado[3] <= 20){
+        return "UberBlack"
+    } else if (resultado[2] <= 20){
+        return "UberPlus"
+    } else if (resultado[1] <= 20){
+        return "UberXL"
+    } else if (resultado[0] <= 20){
+        return "UberX"
+    }
+    
+    return resultado;
+}
